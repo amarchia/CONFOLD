a1dc6fded985a4f666b532082796a36da1a764dc
pabmevi <pabmevi@gmail.com>
2025-09-15 17:24:01 -0500

    s

--- extinction.py
+++ extinction.py
@@ -19,7 +19,7 @@ def extinction():
              "Wing.Length", "LogRangeSize", "LogClutchSize", "LogNightLights", 
              "LogHumanPopulationDensity"]
 
-    model = Classifier(attrs=attrs, numeric=nums, label="status")
+    model = Classifier(attrs=attrs, numeric=nums, label="Migration")
     data = model.load_data('AvoIUCNbehav.csv')
     print('\n% dataset', np.shape(data))
     return model, data
